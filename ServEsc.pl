0d27598668c57db636d33a63c06b1c2389209c5f
Brisa Villarreal <brisa.bluesky@gmail.com>
2017-03-16 19:17:37 -0800

    Agregado codigo

--- ServEsc.pl
+++ ServEsc.pl
@@ -1,7 +1,29 @@
 tramite(beca).
 tramite(constancia).
 tramite(titulacion).
+tramite(serviciosocial).
+tramite(residenciaprofesional).
+
+esuntramite(x) :- tramite(x).
+
 requiere_pago(beca).
 requiere_pago(titulacion).
 requiere_documentos(beca).
 requiere_documentos(titulacion).
+
+
+requiere_documentacion(x) :- requiere_pago(x), requiere_documentos(x).
+
+solicitud(serviciosocial).
+solicitud(residenciaprofesional).
+solicitud(titulacion).
+cartaasignacion(serviciosocial).
+cartadepresentacion(serviciosocial).
+cartadepresentacion(residenciaprofesional).
+cartadeaceptacion(serviciosocial).
+cartadeaceptacion(residenciaprofesional).
+kardex(residenciaprofesional).
+kardex(serviciosocial).
+
+requiere_servicio :- solicitud(x), cartaasignacion(x), cartadepresentacion(x),kardex(x).
+
